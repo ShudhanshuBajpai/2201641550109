e3d15b2615ef280d982f08b5b6d0e021fa26322c
ShudhanshuBajpai <2k22.csiot.2212506@gmail.com>
2025-09-08 14:43:33 +0530

    updated db.js

--- url_shortener/database/db.js
+++ url_shortener/database/db.js
@@ -1,10 +1,21 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import Log from '../middlewares/loggingMiddleware.js';
+
+
 dotenv.config();
 
 const connectionString = process.env.CONNECTION_STRING;
+
 export const connect = () => {
-    mongoose.connect(connectionString)
-        .then(() => console.log('MongoDB connected'))
-        .catch(err => console.log(err));
-}
+  mongoose
+    .connect(connectionString)
+    .then(() => {
+      console.log("MongoDB connected");
+      Log("backend", "info", "db", "MongoDB connected successfully");
+    })
+    .catch((err) => {
+      console.error(err);
+      Log("backend", "fatal", "db", `DB connection failed: ${err.message}`);
+    });
+};
